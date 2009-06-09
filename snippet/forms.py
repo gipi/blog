b976b067bb7060c413f4510c0fe0c8cbda5e5cd7
gianluca.pacchiella <pacchiel@studenti.ph.unito.it>
2009-06-09 10:56:48 +0200

    Changed style of textarea.

--- snippet/forms.py
+++ snippet/forms.py
@@ -2,7 +2,8 @@ from django.forms import ModelForm, Textarea, CharField
 from snippet.models import Entry
 
 class EntryForm(ModelForm):
-    content = CharField(widget=Textarea())
+    content = CharField(widget=Textarea(
+	    attrs={'style': 'width:80em;height:40em;'}))
 
     class Meta:
         model = Entry
