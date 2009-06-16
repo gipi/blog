3ef7c427e1a311b4b12d12b23e2f744beb43d687
gianluca.pacchiella <pacchiel@studenti.ph.unito.it>
2009-06-16 22:15:56 +0200

    Added 'tikz' directive.

--- snippet/rst_tex.py
+++ snippet/rst_tex.py
@@ -15,3 +15,16 @@ def latex_directive(name, arguments, options, content, lineno,
 
 latex_directive.content = 1
 directives.register_directive('latex', latex_directive)
+ 
+def get_tikz_img(formula):
+	print 'DEBUG of formula: ', formula
+	fname = texrender.tikz_render_formula(formula, settings.TEX_MEDIA)
+	return settings.TEX_MEDIA_URL + fname
+
+def tikz_directive(name, arguments, options, content, lineno,
+		content_offset, block_text, state, state_machine):
+	url = get_tikz_img('\n'.join(content))
+	return [nodes.raw('', '<img class="tikz" src="%s" />' % url, format='html')]
+
+tikz_directive.content = 1
+directives.register_directive('tikz', tikz_directive)
--- snippet/texrender.py
+++ snippet/texrender.py
@@ -34,6 +34,42 @@ def wrap_formula(formula, font_size, latex_class):
                \usebox{\formulabox}
                \end{document}""" % locals()
 
+def tikz_wrap_formula(formula):
+    return r"""
+    \input tikz
+    \footline={}
+    \tikzpicture %(formula)s \endtikzpicture
+    \end""" % locals()
+
+def tikz_render_formula(formula, folder, font_size=11, latex_class='article'):
+    hash = sha.new(formula).hexdigest()
+    if os.path.exists(os.path.join(folder, hash + ".png")):
+        return hash + ".png"
+ 
+    tempdir = tempfile.mkdtemp()
+    curpath = os.getcwd()
+    os.chdir(tempdir)
+ 
+    f = file('formula.tex', 'w')
+    f.write(tikz_wrap_formula(formula))
+    f.close()
+ 
+    status = os.system("tex --interaction=nonstopmode formula.tex")
+    assert 0==status, tempdir
+ 
+    status = os.system("dvips -E formula.dvi -o formula.ps")
+    assert 0==status, tempdir
+ 
+    status = os.system("convert -density 120 -trim -transparent \"#FFFFFF\" formula.ps formula.png")
+    assert 0==status, tempdir
+ 
+    os.chdir(curpath)
+ 
+    shutil.copyfile(os.path.join(tempdir, "formula.png"), os.path.join(folder, hash + ".png"))
+    shutil.rmtree(tempdir)
+ 
+    return hash+".png"
+
 def render_formula(formula, folder, font_size=11, latex_class='article'):
     hash = sha.new(formula).hexdigest()
     if os.path.exists(os.path.join(folder, hash + ".png")):
