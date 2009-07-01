2275f85b0e5c6607e53ff1c61077f48356a43f22
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2009-07-01 22:24:20 +0200

    Added 'flatpages' app.

--- settings.py
+++ settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
@@ -77,6 +78,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.markup',
+    'django.contrib.flatpages',
     'snippet',
 )
 
