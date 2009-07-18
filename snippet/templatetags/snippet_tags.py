3795e2d423959d961f87da6ede6a4d23a670a15f
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2009-07-18 12:55:01 +0200

    Added support for gravatar.
    For more info look at <http://gravatar.com>.

---
+++ snippet/templatetags/snippet_tags.py
@@ -0,0 +1,21 @@
+# http://en.gravatar.com/site/implement/python
+# import code for encoding urls and generating md5 hashes
+import urllib, hashlib
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('gravatar.html')
+def gravatar(email, site):
+	size = 40
+
+	# construct the url
+	gravatar_url = "http://www.gravatar.com/avatar.php?"
+	gravatar_url += urllib.urlencode(
+		{
+		'gravatar_id':hashlib.md5(email).hexdigest(),
+		'default':site, 'size':str(size)
+		}
+	)
+
+	return { 'url': gravatar_url }
