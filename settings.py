a84a8b896c15f5813fcd2c57987a877957e9fc6c
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2010-01-12 10:51:35 +0100

    Delete SECRET_KEY.
    Has to be put into 'local_settings.py'.

--- settings.py
+++ settings.py
@@ -40,9 +40,6 @@ TEX_MEDIA_URL = MEDIA_URL + '/TeX/'
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/admin_media/'
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '%*c4rvpyore6oy3n9m43j#gg9jf_u656e$xb8o^b5quzfo298s'
-
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
