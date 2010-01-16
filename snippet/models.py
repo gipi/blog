2f403df04fb3dc6f59df5db4d5cb1c1d6ce3fcc2
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2010-01-16 11:46:47 +0100

    Add 'help_text' to the TagField in the Blog model.
    When I make a new post I never remember if tag are space or comma
    separated

--- snippet/models.py
+++ snippet/models.py
@@ -19,5 +19,5 @@ class Blog(models.Model):
     content = models.CharField(max_length=1000)
     creation_date = models.DateTimeField(auto_now_add=True)
     modify_date = models.DateTimeField(auto_now_add=True)
-    tags = TagField()
+    tags = TagField(help_text='separe tags with commas')
     user = models.ForeignKey(User)
