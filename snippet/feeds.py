4f58e535d610120855260607585c2f0fb66e6c66
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2010-01-19 12:42:10 +0100

    Create a subclass of the django feeds.
    This allows a more DRY code.

--- snippet/feeds.py
+++ snippet/feeds.py
@@ -2,12 +2,14 @@ from django.contrib.syndication.feeds import Feed
 
 from snippet.models import Blog
 
-class LatestBlogEntriesFeed(Feed):
+class BlogFeed(Feed):
+    title_template = 'snippet/feeds_title.html'
+    description_template = 'snippet/feeds_description.html'
+
+class LatestBlogEntriesFeed(BlogFeed):
     title = 'Latest blog post'
     link = '/blog/'
     description = 'Updates sui nuovi post'
-    title_template = 'snippet/feeds_title.html'
-    description_template = 'snippet/feeds_description.html'
 
     def items(self):
         return Blog.objects.order_by('-creation_date')[:5]
