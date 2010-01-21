dbde04c4f4a7ef3ee08beac13e3b3e98b448ddb6
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2010-01-21 11:14:06 +0100

    Add support for month based archive.
    There is only the URL but no link point yet to it.

--- snippet/urls.py
+++ snippet/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import *
+from django.views.generic.date_based import archive_month
+
+from snippet.models import Blog
 
 urlpatterns = patterns('',
                 url(r'^$', 'snippet.views.blog_list', name='blog-list'),
@@ -7,4 +10,10 @@ urlpatterns = patterns('',
                     'snippet.views.blog_add', name='blog-edit'),
                 url(r'^post/([\w\d-]*)/$',
                         'snippet.views.blog_view', name='blog-post'),
+                url(r'^archives/(?P<year>\d{4})/(?P<month>.*)/$',
+                    archive_month, {
+                        'queryset': Blog.objects.all().\
+                                filter(status='pubblicato'),
+                        'date_field': 'creation_date'
+                    }, name='blog-archives'),
 )
