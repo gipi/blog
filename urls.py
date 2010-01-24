d000f12689180801255bd73294d5e63e3d13080e
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2010-01-24 16:28:32 +0100

    Add admin interface.

--- urls.py
+++ urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.views.generic.simple import redirect_to
+from django.contrib import admin
+
+admin.autodiscover()
 
 from snippet.feeds import LatestBlogEntriesFeed, LatestBlogEntriesForUserFeed
 
@@ -10,6 +13,7 @@ feeds = {
 }
 
 urlpatterns = patterns('',
+        (r'^admin/(.*)', admin.site.root),
         url(r'^$', redirect_to, {'url': '/blog/'}, name='home'),
         url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
         url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
