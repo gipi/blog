9c293031577ef12a21a63194062c7e825da32466
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2010-02-25 11:40:17 +0100

    More space is allowed in blog's post.

--- yadb/models.py
+++ yadb/models.py
@@ -16,7 +16,7 @@ class Blog(models.Model):
             ('pubblicato', 'pubblicato'),
         )
     )
-    content = models.CharField(max_length=1000)
+    content = models.CharField(max_length=10000)
     creation_date = models.DateTimeField(auto_now_add=True)
     modify_date = models.DateTimeField(auto_now_add=True)
     tags = TagField(help_text='separe tags with commas')
