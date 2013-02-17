edb63d712c3ba5073c9bd1a0c2200bafb51fdc62
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-02-17 21:33:39 +0100

    Change ADMIN_MEDIA_PREFIX.

--- settings.py
+++ settings.py
@@ -45,7 +45,7 @@ UPLOAD_URL = MEDIA_URL + '/uploads/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/admin_media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
